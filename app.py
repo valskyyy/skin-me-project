619da18e958aca81fb38ed083ca51464c8902d6a
valskyyy <valentinlew@gmail.com>
2021-11-24 11:57:04 +0100

    modifs micro

--- app.py
+++ app.py
@@ -28,11 +28,7 @@ genre = st.radio(
 "Vous êtes",
 ('Un homme', 'Une femme'))
 
-age = st.select_slider(
-'Votre âge',
-options=['0 à 4 ans', '5 à 9 ans', '10 à 14 ans', '15 à 19 ans', '20 à 24 ans', '25 à 29 ans', '30 à 34 ans',
-'35 à 39 ans','40 à 44 ans','45 à 49 ans','50 à 54 ans','55 à 59 ans',
-'60 à 64 ans','65 à 69 ans','70 à 74 ans','75 à 79 ans','80 à 84 ans',])
+age = st.text_input(label = 'Votre âge', placeholder="ex : 34, 60...")
 
 
 genre = st.radio(
